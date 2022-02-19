e5dafcaad0365067721cd94b2b306a23ce0b31de
Mrinmoy Sarkar <msarkar07@live.in>
2022-02-19 16:58:21 +0530

    Initial commit

---
+++ new.js
@@ -0,0 +1,51 @@
+// function outermost(){
+//     var c = 20;
+//     function outer(b){
+//         function inner(){
+//             console.log(a,b,c)
+//         }
+//         let a = 10;
+//         return inner;
+//     }
+//     return outer;
+//
+// }
+// var close = outermost()("hello world")
+// close();
+
+//callback event listner
+
+// function addEvent() {
+//     let counter = 0;
+//     document.getElementById('clickMeBroo').addEventListener('click', function xyz () {
+//         console.log('CLicked', ++counter)
+//     });
+// }
+//
+// addEvent();
+
+//higher order function
+// radius= [4,5,6]
+// const area = function (value) {
+//     return Math.PI * value* value
+// }
+//
+// console.log(radius.map(area))
+
+//prototype
+
+//bind call apply
+
+const object = {
+    name: "Tony Soprano",
+    place: "New Jersey"
+}
+
+let printName = function (series,realName) {
+    console.log(this.name + ' '+ this.place + ' ' + series + ' ' + realName)
+}
+
+printName.call(object, "Sopranos", "Dont know")
+printName.apply(object, ["Sopranos", "Dont know"])
+bindExample = printName.bind(object)
+bindExample()
