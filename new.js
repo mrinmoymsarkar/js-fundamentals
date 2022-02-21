dc5daf0910a5c98ebcabb44563839362f3c9b884
Mrinmoy Sarkar <msarkar07@live.in>
2022-02-21 14:01:30 +0530

    memoization

--- new.js
+++ new.js
@@ -51,25 +51,75 @@
 // bindExample()
 
 //pollyfill for bind
+//
+// let names = {
+//     firstName: 'Chirs',
+//     lastName: 'Moltisanti'
+// }
 
-let names = {
-    firstName: 'Chirs',
-    lastName: 'Moltisanti'
-}
+// let printShow = function (showName, showName2) {
+//     console.log(this.firstName + ' ' + this.lastName + ' ' + showName + ' ' + showName2 )
+// }
+//
+// Function.prototype.myBind = function (...args){
+//    let obj = this,
+//    params = args.slice(1);
+//     return function (...args2){
+//        console.log()
+//         return obj.apply(args[0],[...params,...args2])
+//     }
+// }
+//
+// let printName = printShow.myBind(names,"Sopranos", )
+// printName("Kim's Convinience");
 
-let printShow = function (showName, showName2) {
-    console.log(this.firstName + ' ' + this.lastName + ' ' + showName + ' ' + showName2 )
-}
+//Currying
 
-Function.prototype.myBind = function (...args){
-   let obj = this,
-   params = args.slice(1);
-    return function (...args2){
-       console.log()
-        return obj.apply(args[0],[...params,...args2])
+// by closure
+
+// let addition = function (x) {
+//     return function (y) {
+//         return console.log(x * y)
+//     }
+// }
+//
+// let additionBySix = addition(6)
+// additionBySix(5)
+//
+// //by binding
+//
+// let additionBind = function (x, y) {
+//         return console.log(x * y)
+// }
+//
+// let additionByTen = additionBind.bind(this, 10)
+// additionByTen(10);
+
+//fibonacci
+
+function memoFibonacci(n, memo) {
+    memo = memo || {}
+    if(memo[n]){
+        console.log('[n]', n)
+        console.log('memo[n]', memo[n])
+        return memo[n]
+
     }
+    if(n<=1){
+        return 1
+    }
+
+    return memo[n] = memoFibonacci(n-1, memo)+ memoFibonacci(n-2, memo)
 }
 
-let printName = printShow.myBind(names,"Sopranos", )
-printName("Kim's Convinience");
+console.log(memoFibonacci(10))
 
+// function fibonacci(n) {
+//     if(n<=1){
+//         return 1
+//     }
+//
+//     return fibonacci(n-1)+ fibonacci(n-2)
+// }
+//
+// console.log(fibonacci(100))
