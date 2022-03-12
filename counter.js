23761935a2dd9e831b725eb19c6ff7a785d6efb5
mrinmoymsarkar <msarkar07@live.in>
2022-03-12 17:09:51 +0530

    Update counter.js

--- counter.js
+++ counter.js
@@ -4,3 +4,16 @@ let counter = 0;
 return function (){
 return ++counter
 }})()
+
+//fix for loop setitmeout fix
+function forLoop() {
+    for (var i = 0; i <= 2; i++) {
+         console.log('What is this')
+        function fix(i){
+        setTimeout(()=>{
+            console.log(i)
+        }, 0)
+    }
+        fix(i)
+    }
+}
